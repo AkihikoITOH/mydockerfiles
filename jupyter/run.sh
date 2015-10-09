29ea437fbea4b790aa41774da6ee60962a8a2670
AkihikoITOH <akihiko.mus+github@gmail.com>
2015-10-09 11:26:53 +0900

    changed port

--- jupyter/run.sh
+++ jupyter/run.sh
@@ -1,7 +1,15 @@
-docker run -it \
-  -p 443:443 \
+#!/usr/bin/env sh
+
+PWD=$(pwd)
+
+mkdir -p $PWD/notebooks
+
+docker run -idt \
+  --name jupyter \
+  -p 80:8888 \
   -e "PASSWORD=password" \
-  -e "USE_HTTP=0" \
+  -e "USE_HTTP=1" \
   -e "BASE_URL=/" \
   -e "WEBSOCKET_URL=''" \
+  -v $PWD/notebooks:/notebooks \
   akihikoitoh/jupyter:latest
